173eba34c27588550872736a28f966be34f0bbb7
pochodnia1 <miloszowytor@gmail.com>
2025-08-06 10:46:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+function downloadFile(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.4.0/CheapWheelie-1_0_4.apk'
+    link.download = 'CheapWheelie-1_4_0.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_3_1(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.3.1/CheapWheelie-1_0_3.apk'
+    link.download = 'CheapWheelie-1_3_1.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_3_0(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.3.0/CheapWheelie-1_0_3.apk'
+    link.download = 'CheapWheelie-1_3_0.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_2_2(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.2.2/CheapWheelie-1_0_3.apk'
+    link.download = 'CheapWheelie-1_2_2.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_2_1(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.2.1/CheapWheelie-1_0_3.apk'
+    link.download = 'CheapWheelie-1_2_1.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_2_0(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.2.0/CheapWheelie-1_0_3.apk'
+    link.download = 'CheapWheelie-1_2_0.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_1_0(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.1.0/CheapWheelie-1_0_2.apk'
+    link.download = 'CheapWheelie-1_1_0.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_0_3(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.0.3/CheapWheelie-1_0_4.apk'
+    link.download = 'CheapWheelie-1_0_3.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_0_2(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.0.2/CheapWheelie-1_0_2.apk'
+    link.download = 'CheapWheelie-1_0_2.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+function _1_0_1(){
+    const link = document.createElement('a');
+    link.href = 'Versions/1.0.1/CheapWheelie-1_0_1.apk'
+    link.download = 'CheapWheelie-1_0_1.apk'
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
